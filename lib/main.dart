9505c127048cca44be5cea4bf85d519da4842203
kentos731 <r119320@g.kobe-kosen.ac.jp>
2023-04-06 16:29:20 +0900

    wktyyの分をコミットしますbykentos

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key.key});
 
   // This widget is the root of your application.
   @override
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+  const MyHomePage({Key.key, required this.title});
   final String title;
   @override
   State<MyHomePage> createState() => _MyHomePageState();
@@ -37,7 +37,121 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   @override
+  Widget build(Buil//非同期処理用ライブラリ
+import 'dart:async';
+//ファイル出力用ライブラリ
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+//アプリがファイルを保存可能な場所を取得するライブラリ
+import 'package:path_provider/path_provider.dart';
+
+//テキストフィールドの状態を管理するためのクラス
+final _textController = TextEditingController();
+//出力するテキストファイル名
+final _fileName = 'editTextField.txt';
+
+void main() => runApp(MyApp());
+
+//ステートレス
+class MyApp extends StatelessWidget {
+  @override
   Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter ファイル 出力テスト',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter ファイル 出力テスト'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+//ステートフル
+class _MyHomePageState extends State<MyHomePage> {
+  //読み込んだテキストファイルを出力
+  String _out = '';
+
+  //ファイルの出力処理
+  void outButton() async {
+    getFilePath().then((File file) {
+      file.writeAsString(_textController.text);
+    });
+  }
+
+  //ファイルの読み込みと変数への格納処理
+  void loadButton() async {
+    setState(() {
+      load().then((String value) {
+        setState(() {
+          _out = value;
+        });
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('テキストを入力してください'),
+            Padding(
+              padding: EdgeInsets.all(10.0),
+              child: TextField(
+                autofocus: true,
+                controller: _textController,
+                decoration: InputDecoration(icon: Icon(Icons.arrow_forward)),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(10.0),
+              //ファイル出力用のボタン(ボタン押下でoutButtonメソッドを呼び出し)
+              child:RaisedButton(child: Text('ファイルに出力する'), onPressed: outButton),
+            ),
+            Padding(
+              padding: EdgeInsets.all(10.0),
+              //ファイル読み込み用のボタン(ボタン押下でloadButtonメソッドを呼び出し)
+              child: RaisedButton(
+                  child: Text('出力したファイルを読み込む'), onPressed: loadButton),
+            ),
+            Padding(
+                padding: EdgeInsets.all(10.0),
+                //読み込みだファイルの内容を表示
+                child: Text(
+                  '出力したファイルの内容は「' + _out + '」です！',
+                )),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+//テキストファイルを保存するパスを取得する
+Future<File> getFilePath() async {
+  final directory = await getTemporaryDirectory();
+  return File(directory.path + '/' + _fileName);
+}
+
+//テキストファイルの読み込み
+Future<String> load() async {
+  final file = await getFilePath();
+  return file.readAsString();
+}dContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
---
+++ test/filechange_test.dart
@@ -0,0 +1,118 @@
+import 'dart:async'; //非同期処理用ライブラリ
+import 'dart:io';  //ファイル出力用ライブラリ
+import 'package:path_provider/path_provider.dart'; //アプリがファイルを保存可能な場所を取得するライブラリ
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget { 
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'otameshi',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('OutputTest'),
+        ),
+        body: OutputTest(),
+      ),
+    );
+  }
+}
+
+class OutputTest extends StatefulWidget{
+  @override
+  _OutputTestState createState() => new _OutputTestState();
+}
+
+class _OutputTestState extends State<OutputTest>{
+
+  // 入力された内容を保持するコントローラ
+  final outputController = TextEditingController();
+
+  // 表示用の変数
+  String inputText = "0回、出力しました";
+  int inputNum = 0;
+
+  // 「ファイルに出力する」が押されたときの処理
+  void output(String s) async{
+    setState(() {
+      ++inputNum;
+      inputText = "" + inputNum.toString() + "回、出力しました";
+    });
+    
+    getFilePath().then((File file) { //thenの記述で非同期処理であるgetFilePath関数の処理を待っている
+      file.writeAsString(s);
+    });
+  }
+
+  //ファイルの読み込みと変数への格納処理
+  void loadFile() async {
+    setState(() {
+      load().then((String value) {
+        setState(() {
+          inputText = value;
+        });
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: <Widget>[
+        // Column1
+        Container(
+          alignment: Alignment.center,
+          child: Container(
+            width: 300,
+            height: 100,
+            child: TextField(
+              enabled: true, //活性or非活性
+              maxLength: 10, //入力最大文字数
+              style: TextStyle(color: Colors.red), //入力文字のスタイル
+              obscureText: false, //trueでマスク（****表記）にする
+              maxLines:1, //入力可能行数
+              controller: outputController,
+            ),
+          )
+        ),
+
+        // Column2
+        GestureDetector(
+          onTap: () {
+            output(outputController.text);
+          },
+          child: Text("ファイルに出力する"),
+        ),
+
+        // Column3
+        GestureDetector(
+          onTap: () {
+            loadFile();
+          },
+          child: Text("ファイルを読み込む"),
+        ),
+
+        // Column4
+        Text(inputText),
+
+      ],
+    );
+  }
+}
+
+//テキストファイルを保存するパスを取得する
+Future<File> getFilePath() async { //Future<T> 関数名 asyncで<T>クラスを扱いとする非同期処理をする関数。非同期処理は、実行されると、その終了を待たずに他の処理が実行されます。関数内に１つでも非同期処理が実行される場合は非同期関数となります。
+  final directory = await getTemporaryDirectory(); //await はその処理が終わるまで待つということ
+  debugPrint(directory.path);
+  return File(directory.path + '/test.txt');
+}
+
+//テキストファイルの読み込み
+Future<String> load() async {
+  final file = await getFilePath();
+  return file.readAsString();
+}
